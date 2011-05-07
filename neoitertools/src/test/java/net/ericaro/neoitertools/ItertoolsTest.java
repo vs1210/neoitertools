4b8f606c1a4134b1ccd43b6dcc06c675c5ec00d8
ericaro <ericaro@gmail.com>
2011-05-07 09:21:42 +0000

    little formatting change (due to autoformat)

--- neoitertools/src/test/java/net/ericaro/neoitertools/ItertoolsTest.java
+++ neoitertools/src/test/java/net/ericaro/neoitertools/ItertoolsTest.java
@@ -17,10 +17,15 @@ public class ItertoolsTest {
 		}
 		System.out.println("...");
 	}
-	public static Lambda<Integer, Boolean> lt(final int bound){
-		return new Lambda<Integer,Boolean>(){public Boolean map(Integer x) {return x<bound;}};
+
+	public static Lambda<Integer, Boolean> lt(final int bound) {
+		return new Lambda<Integer, Boolean>() {
+			public Boolean map(Integer x) {
+				return x < bound;
+			}
+		};
 	}
-	
+
 	Lambda<? super List<Character>, String> str = new Lambda<List<Character>, String>() {
 
 		public String map(List<Character> arg) {
@@ -77,13 +82,13 @@ public class ItertoolsTest {
 	@Test
 	public void testCount() {
 		System.out.println(">>> count();");
-		assertList(count(), 0,1,2,3,4 );
+		assertList(count(), 0, 1, 2, 3, 4);
 	}
 
 	@Test
 	public void testCountInt() {
 		System.out.println(">>> count(6);");
-		assertList(count(6), 6,7,8,9 );
+		assertList(count(6), 6, 7, 8, 9);
 	}
 
 	@Test
@@ -97,51 +102,51 @@ public class ItertoolsTest {
 	public void testDropwhile() {
 		// dropwhile(lambda x: x<5, [1,4,6,4,1]) --> 6 4 1
 		System.out.println(">>> dropwhile(lt(5), iter(binomial) )");
-		int[] binomial = new int[]{1,4,6,4,1};
-		
-		assertList(dropwhile(lt(5), iter(binomial) ), 6,4,1);
+		int[] binomial = new int[] { 1, 4, 6, 4, 1 };
+
+		assertList(dropwhile(lt(5), iter(binomial)), 6, 4, 1);
 	}
 
 	@Test
 	public void testEnumerate() {
-//		>>> for i, season in enumerate(['Spring', 'Summer', 'Fall', 'Winter']):
-//			...     print i, season
-//			0 Spring
-//			1 Summer
-//			2 Fall
-//			3 Winter
-		System.out.println(">>> for(Index<Character>  pair: in( enumerate(iter(\"ABCD\")))){\n" + 
-				"...     System.out.println(pair.i+\" \"+pair.value);");
-		for(Index<Character>  pair: in( enumerate(iter("ABCD")))){
-			System.out.println(pair.i+" "+pair.value);
+		// >>> for i, season in enumerate(['Spring', 'Summer', 'Fall', 'Winter']):
+		// ... print i, season
+		// 0 Spring
+		// 1 Summer
+		// 2 Fall
+		// 3 Winter
+		System.out.println(">>> for(Index<Character>  pair: in( enumerate(iter(\"ABCD\")))){\n" + "...     System.out.println(pair.i+\" \"+pair.value);");
+		for (Index<Character> pair : in(enumerate(iter("ABCD")))) {
+			System.out.println(pair.i + " " + pair.value);
 		}
 	}
 
 	@Test
 	public void testFilter() {
 		System.out.println(">>> filter(pair, range(10))");
-		//ifilter(lambda x: x%2, range(10)) --> 1 3 5 7 9
-		assertList(filter(pair, range(10)), 1,3,5,7,9);
+		// ifilter(lambda x: x%2, range(10)) --> 1 3 5 7 9
+		assertList(filter(pair, range(10)), 1, 3, 5, 7, 9);
 	}
 
 	@Test
 	public void testFilterfalse() {
 		System.out.println(">>> filterfalse(pair, range(10))");
-		//ifilter(lambda x: x%2, range(10)) --> 1 3 5 7 9
-		assertList(filterfalse(pair, range(10)), 0,2,4,6,8);
+		// ifilter(lambda x: x%2, range(10)) --> 1 3 5 7 9
+		assertList(filterfalse(pair, range(10)), 0, 2, 4, 6, 8);
 	}
 
 	@Test
 	public void testGroupby() {
-		//[k for k, g in groupby('AAAABBBCCDAABBB')] --> A B C D A B
-		Lambda<Pair<Character,Generator<Character>>,Character> key = new Lambda<Pair<Character,Generator<Character>>,Character>(){
+		// [k for k, g in groupby('AAAABBBCCDAABBB')] --> A B C D A B
+		Lambda<Pair<Character, Generator<Character>>, Character> key = new Lambda<Pair<Character, Generator<Character>>, Character>() {
 			public Character map(Pair<Character, Generator<Character>> arg) {
 				return arg.f0;
-			}}; 
-			Lambda<Character, Character> id = identity();
-			System.out.println(">>> map(key, groupby(iter(\"AAABBBCCDAABBB\"), id))");
-			
-		assertList(map(key, groupby(iter("AAABBBCCDAABBB"), id)), 'A','B','C', 'D', 'A', 'B');
+			}
+		};
+		Lambda<Character, Character> id = identity();
+		System.out.println(">>> map(key, groupby(iter(\"AAABBBCCDAABBB\"), id))");
+
+		assertList(map(key, groupby(iter("AAABBBCCDAABBB"), id)), 'A', 'B', 'C', 'D', 'A', 'B');
 	}
 
 	@Test
@@ -152,39 +157,36 @@ public class ItertoolsTest {
 
 	@Test
 	public void testIn() {
-		
-		System.out.println(">>> for(int i : in(range(3)))\n" + 
-				"...			System.out.println(\"3\");");
-		for(int i : in(range(3)))
+
+		System.out.println(">>> for(int i : in(range(3)))\n" + "...			System.out.println(\"i\");");
+		for (int i : in(range(3)))
 			System.out.println(i);
 	}
 
 	@Test
 	public void testIterBooleanArray() {
-		boolean[] val = new boolean[]{true, false} ;
+		boolean[] val = new boolean[] { true, false };
 		List<Boolean> list = list(iter(val));
-		System.out.println(list);
-		for(Index<Boolean> pair: in( enumerate(iter(val))) )
-			assert val[pair.i] == pair.value ;
+		//System.out.println(list);
+		for (Index<Boolean> pair : in(enumerate(iter(val))))
+			assert val[pair.i] == pair.value;
 	}
 
 	@Test
 	public void testIterByteArray() {
-		byte[] val = new byte[]{1, 2} ;
+		byte[] val = new byte[] { 1, 2 };
 		List<Byte> list = list(iter(val));
 		System.out.println(list);
-		for(Index<Byte> pair: in( enumerate(iter(val))) )
-			assert val[pair.i] == pair.value ;
+		for (Index<Byte> pair : in(enumerate(iter(val))))
+			assert val[pair.i] == pair.value;
 	}
 
-	
 	@Test
 	public void testIterCharSequence() {
-		System.out.println(">>> for(Character c: in( iter(\"ABCD\")))\n" + 
-				"...		System.out.println(c);");
-		for(Character c: in( iter("ABCD")))
+		System.out.println(">>> for(Character c: in( iter(\"ABCD\")))\n" + "...		System.out.println(c);");
+		for (Character c : in(iter("ABCD")))
 			System.out.println(c);
-			
+
 	}
 
 	@Test
@@ -192,191 +194,188 @@ public class ItertoolsTest {
 		// equivalent to count
 		Yield<Void, Integer> yieldFunction = new Yield<Void, Integer>() {
 			public void generate() {
-				int i=0;
-				while(true) 
+				int i = 0;
+				while (true)
 					yield(i++);
 			}
 		};
 		System.out.println(">>> iter(yieldFunction);");
-		assertList(iter(yieldFunction), 0,1,2,3,4,5);
-		
+		assertList(iter(yieldFunction), 0, 1, 2, 3, 4, 5);
+
 	}
 
 	@Test
 	public void testListGeneratorOfT() {
-		System.out.println(">>> list(range(3)) ;"); 
+		System.out.println(">>> list(range(3)) ;");
 		System.out.println(list(range(3)));
 	}
 
 	@Test
 	public void testListGeneratorOfTInt() {
-		System.out.println(">>> list(count(),3) ;"); 
-		System.out.println(list(count(),3) );
+		System.out.println(">>> list(count(),3) ;");
+		System.out.println(list(count(), 3));
 	}
 
 	@Test
 	public void testMap() {
 		System.out.println(">>> map(pair,range(10) );");
-		assertList(map(pair,range(10) ), true, false, true, false);
-		
+		assertList(map(pair, range(10)), true, false, true, false);
+
 	}
 
-//	@Test
-//	public void testPermutationsGeneratorOfT() {
-//		System.out.println(">>> ");
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testPermutationsGeneratorOfTInt() {
-//		System.out.println(">>> ");
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testProductGeneratorOfGeneratorOfT() {
-//		System.out.println(">>> ");
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testProductGeneratorOfGeneratorOfTInt() {
-//		System.out.println(">>> ");
-//		fail("Not yet implemented");
-//	}
+	// @Test
+	// public void testPermutationsGeneratorOfT() {
+	// System.out.println(">>> ");
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testPermutationsGeneratorOfTInt() {
+	// System.out.println(">>> ");
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testProductGeneratorOfGeneratorOfT() {
+	// System.out.println(">>> ");
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testProductGeneratorOfGeneratorOfTInt() {
+	// System.out.println(">>> ");
+	// fail("Not yet implemented");
+	// }
 
 	@Test
 	public void testRangeInt() {
-		System.out.println(">>> range(3)"); 
-		assertList(range(3), 0,1,2);
+		System.out.println(">>> range(3)");
+		assertList(range(3), 0, 1, 2);
 	}
 
 	@Test
 	public void testRangeIntInt() {
-		System.out.println(">>> range(1,3)"); 
-		assertList(range(1,3), 1,2);
+		System.out.println(">>> range(1,3)");
+		assertList(range(1, 3), 1, 2);
 	}
 
 	@Test
 	public void testRangeIntIntInt() {
-		System.out.println(">>> range(1,10,5)"); 
-		assertList(range(1,10,5), 1,6);
+		System.out.println(">>> range(1,10,5)");
+		assertList(range(1, 10, 5), 1, 6);
 	}
 
-//	@Test
-//	public void testReduceOperatorOfTGeneratorOfT() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testReduceOperatorOfTGeneratorOfTT() {
-//		fail("Not yet implemented");
-//	}
+	// @Test
+	// public void testReduceOperatorOfTGeneratorOfT() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testReduceOperatorOfTGeneratorOfTT() {
+	// fail("Not yet implemented");
+	// }
 
 	@Test
 	public void testRepeatT() {
 		System.out.println(">>> slice(repeat(\"Hello\"), 5)");
-		assertList(slice(repeat("hello"), 5), "hello","hello","hello","hello","hello");
+		assertList(slice(repeat("hello"), 5), "hello", "hello", "hello", "hello", "hello");
 	}
 
 	@Test
 	public void testRepeatTInt() {
 		System.out.println(">>> repeat(\"Hello\", 5)");
-		assertList(repeat("hello", 5), "hello","hello","hello","hello","hello");
+		assertList(repeat("hello", 5), "hello", "hello", "hello", "hello", "hello");
 	}
 
 	@Test
 	public void testReversed() {
 		System.out.println(">>> reversed(range(5))");
-		assertList(reversed(range(5)), 4,3,2,1,0);
+		assertList(reversed(range(5)), 4, 3, 2, 1, 0);
 	}
 
 	@Test
 	public void testSliceGeneratorOfTInt() {
 		System.out.println(">>> slice(count(),5)");
-		assertList(slice(count(),5), 0,1,2,3,4);
+		assertList(slice(count(), 5), 0, 1, 2, 3, 4);
 	}
 
 	@Test
 	public void testSliceGeneratorOfTIntInt() {
 		System.out.println(">>> slice(count(),3,5)");
-		assertList(slice(count(),3,5), 3,4);
+		assertList(slice(count(), 3, 5), 3, 4);
 	}
 
 	@Test
 	public void testSliceGeneratorOfTIntIntInt() {
 		System.out.println(">>> slice(count(),1,6,2)");
-		assertList(slice(count(),1,6,2), 1,3,5);
+		assertList(slice(count(), 1, 6, 2), 1, 3, 5);
 	}
 
 	@Test
 	public void testSortedGeneratorOfT() {
 		System.out.println(">>> sorted(iter(\"ACBED\"))");
-		assertList(sorted(iter("ACBED")), 'A','B','C','D','E');
+		assertList(sorted(iter("ACBED")), 'A', 'B', 'C', 'D', 'E');
 	}
 
-//	@Test
-//	public void testSortedGeneratorOfTComparatorOfQsuperKLambdaOfQsuperTKBoolean() {
-//		System.out.println(">>> ");
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSortedGeneratorOfTComparatorOfQsuperT() {
-//		System.out.println(">>> ");
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSortedGeneratorOfTLambdaOfTKBoolean() {
-//		System.out.println(">>> ");
-//		fail("Not yet implemented");
-//	}
+	// @Test
+	// public void testSortedGeneratorOfTComparatorOfQsuperKLambdaOfQsuperTKBoolean() {
+	// System.out.println(">>> ");
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testSortedGeneratorOfTComparatorOfQsuperT() {
+	// System.out.println(">>> ");
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testSortedGeneratorOfTLambdaOfTKBoolean() {
+	// System.out.println(">>> ");
+	// fail("Not yet implemented");
+	// }
 
-//	@Test
-//	public void testString() {
-//		System.out.println(">>> ");
-//		fail("Not yet implemented");
-//	}
+	// @Test
+	// public void testString() {
+	// System.out.println(">>> ");
+	// fail("Not yet implemented");
+	// }
 
-//	@Test
-//	public void testStringBuilder() {
-//		System.out.println(">>> ");
-//		fail("Not yet implemented");
-//	}
+	// @Test
+	// public void testStringBuilder() {
+	// System.out.println(">>> ");
+	// fail("Not yet implemented");
+	// }
 
 	@Test
 	public void testTakewhile() {
 		System.out.println(">>> takewhile(count(), lt(5))");
-		assertList(takewhile(count(), lt(5)), 0,1,2,3,4);
+		assertList(takewhile(count(), lt(5)), 0, 1, 2, 3, 4);
 	}
 
-//	@Test
-//	public void testTee() {
-//		System.out.println(">>> ");
-//		fail("Not yet implemented");
-//	}
+	// @Test
+	// public void testTee() {
+	// System.out.println(">>> ");
+	// fail("Not yet implemented");
+	// }
 
-//	@Test
-//	public void testTuple() {
-//		System.out.println(">>> ");
-//		fail("Not yet implemented");
-//	}
+	// @Test
+	// public void testTuple() {
+	// System.out.println(">>> ");
+	// fail("Not yet implemented");
+	// }
 
-	
-
-//	@Test
-//	public void testZipGeneratorOfGeneratorOfT() {
-//		System.out.println(">>> ");
-//		fail("Not yet implemented");
-//	}
+	// @Test
+	// public void testZipGeneratorOfGeneratorOfT() {
+	// System.out.println(">>> ");
+	// fail("Not yet implemented");
+	// }
 
 	@Test
 	public void testZipGeneratorOfT1GeneratorOfT2() {
-		System.out.println(">>> for( Pair<Integer, Character>  p: in( zip(range(4), iter(\"ABCDE\") )))\n" + 
-				"...		System.out.println(p.f0+\" -> \"+ p.f1);");
-		for( Pair<Integer, Character>  p: in( zip(range(4), iter("ABCDE") )))
-			System.out.println(p.f0+" -> "+ p.f1);
+		System.out.println(">>> for( Pair<Integer, Character>  p: in( zip(range(4), iter(\"ABCDE\") )))\n" + "...		System.out.println(p.f0+\" -> \"+ p.f1);");
+		for (Pair<Integer, Character> p : in(zip(range(4), iter("ABCDE"))))
+			System.out.println(p.f0 + " -> " + p.f1);
 	}
 
 }
